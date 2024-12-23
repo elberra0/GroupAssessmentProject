9711652e12a3e23680bff60bbe09f0458f391914
alejandroberraondosoria <elberra0@gmail.com>
2024-12-23 12:11:31 +0100

    First commit
    Log in and Sign in layout colors and images added

---
+++ app/src/main/java/com/example/groupassessmentproject/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.example.groupassessmentproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import android.os.Bundle
+import com.example.groupassessmentproject.databinding.ActivitySplashScreenBinding
+
+class SplashScreen : AppCompatActivity(){
+    private val view by lazy {
+        ActivitySplashScreenBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplash = installSplashScreen()
+        super.onCreate(savedInstanceState)
+        setContentView(view.root)
+
+        screenSplash.setKeepOnScreenCondition{true}
+
+        val intent = Intent(this,LoginRegisterActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
